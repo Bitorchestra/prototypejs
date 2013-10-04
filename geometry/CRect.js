928e93cc5469fb4dd3081080ff3b893451bce379
Emiliano Leporati <ubik@gamezoo.it>
2013-10-04 17:39:02 +0200

    construction and parameter support enhancement; docs

--- geometry/CRect.js
+++ geometry/CRect.js
@@ -2,256 +2,264 @@
 * RECT
 */
 var CRect = Class.create(CClonable, {
-	initialize: function () {
-		var a = $A(arguments);
-		switch (a.length) {
-			case 1:
-				// element          
-				var e = $(a[0]);
-				var p = e.cumulativeOffset();
-				//console.log("P:    ", p.top);
-				//p.left += parseFloat(e.getStyle('left') || '0');
-				//p.top += parseFloat(e.getStyle('top') || '0');
-				var d = e.getDimensions();
-				//d.width += parseFloat(e.getStyle('right') || '0');
-				//d.height += parseFloat(e.getStyle('bottom') || '0');
-				this.left = p.left;
-				this.top = p.top;
-				this.right = p.left + d.width;
-				this.bottom = p.top + d.height;
-				this.width = d.width;
-				this.height = d.height;
-				break;
+    initialize: function () {
+        var a = $A(arguments);
+        switch (a.length) {
+            case 1:
+                // element, position, dimensions
+                var arg = a[0], e = $(arg), p = null, d = null;
+                if (e && e.nodeType) {
+                    // it's an element
+                    p = e.cumulativeOffset();
+                    d = e.getDimensions();
+                } else if (['left', 'top', 'width', 'height'].all(function (p) {
+                    return arg.hasOwnProperty(p);
+                })) {
+                    // it's a CRect or similar
+                    p = d = arg;
+                } else {
+                    throw "Invalid construction arguments: {0}".printf(a);
+                }
+                this.left = p.left;
+                this.top = p.top;
+                this.right = p.left + d.width;
+                this.bottom = p.top + d.height;
+                this.width = d.width;
+                this.height = d.height;
+                break;
 
-			case 2:
-				// [left,top], [width,height]
-				// height, width
-				if (Object.isArray(a[0])) {
-					this.left = a[0][0];
-					this.top = a[0][1];
-				} else {
-					this.left = this.top = 0;
-					this.right = this.width = a[0];
-				}
-				if (Object.isArray(a[1])) {
-					this.right = this.left + (this.width = a[1][0]);
-					this.bottom = this.top + (this.height = a[1][1]);
-				} else {
-					this.bottom = this.height = a[1];
-				}
-				break;
+            case 2:
+                // [left,top], [width,height]
+                // height, width
+                // p0, p1
+                if (Object.isArray(a[0]) != Object.isArray(a[1]))
+                    throw "Invalid construction arguments: {0}".printf(a);
+                
+                if (Object.isArray(a[0])) {
+                    this.left = a[0][0];
+                    this.top = a[0][1];
+                    this.right = this.left + (this.width = a[1][0]);
+                    this.bottom = this.top + (this.height = a[1][1]);
+                } else if (a.all(function(arg) {
+                        return ['x','y'].all(function(p) {
+                            return arg.hasOwnProperty(p);
+                        });
+                    })) {
+                    this.left = a[0].x;
+                    this.top = a[0].y;
+                    this.right = a[1].x;
+                    this.bottom = a[1].y;
+                    this.width = this.right - this.left;
+                    this.height = this.bottom - this.top;
+                } else {
+                    this.left = this.top = 0;
+                    this.right = this.width = a[0];
+                    this.bottom = this.height = a[1];
+                }
+                break;
 
-			case 4:
-				// left, top, right, bottom
-				this.left = a[0];
-				this.top = a[1];
-				this.right = a[2];
-				this.bottom = a[3];
-				this.width = this.right - this.left;
-				this.height = this.bottom - this.top;
-				break;
+            case 4:
+                // left, top, right, bottom
+                this.left = a[0];
+                this.top = a[1];
+                this.right = a[2];
+                this.bottom = a[3];
+                this.width = this.right - this.left;
+                this.height = this.bottom - this.top;
+                break;
 
-			default:
-				console.debug(a);
-				throw "Invalid construction arguments";
-		}
+            default:
+                throw "Invalid construction arguments: {0}".printf(a);
+        }
+    }
+    , toString: function () {
+        var round = function (x) { return Math.round(x * 100) / 100; };
+        return "R[[{0},{1}],[{2},{3}]]".printf(round(this.left), round(this.top), round(this.right), round(this.bottom));
+    }
+    , isValid: function () {
+        return (this.width >= 0 && this.height >= 0);
+    }
+    , getLeftTop: function () {
+        return new CPoint(this.left, this.top);
+    }
+    , getLeftBottom: function () {
+        return new CPoint(this.left, this.bottom);
+    }
+    , getRightTop: function () {
+        return new CPoint(this.right, this.top);
+    }
+    , getRightBottom: function () {
+        return new CPoint(this.right, this.bottom);
+    }
+    , getCenter: function () {
+        return new CPoint(this.left + this.width / 2, this.top + this.height / 2);
+    }
+    , getDimensions: function () {
+        return new CPoint(this.width, this.height);
+    }
+    , doesContainPoint: function (p, tolerance) {
+        tolerance = tolerance || 0;
+        var tmp = new CPoint(p);
+        return (tmp.x - this.left >= tolerance && this.right - tmp.x >= tolerance)
+            && (tmp.y - this.right >= tolerance && this.bottom - tmp.y >= tolerance)
+    }
+    , doesContainRect: function (r, tolerance) {
+        var tmp = new CRect(r);
+        return this.doesContainPoint(tmp.getLeftTop(), tolerance)
+            && this.doesContainPoint(tmp.getRightBottom(), tolerance);
+    }
 
-		if (this.height < 0 || this.width < 0) {
-			throw ("Invalid size " + this.width + "x" + this.height);
-		}
-	}
-	, toString: function () {
-		var round = function (x) { return Math.round(x * 100) / 100; };
-		return "[L" + round(this.left) + " T" + round(this.top) + " R" + round(this.right) + " B" + round(this.bottom) + "]";
-	}
-	, isValid: function () {
-		return (this.width >= 0 && this.height >= 0);
-	}
-	, getLeftTop: function () {
-		return new CPoint(this.left, this.top);
-	}
-	, getLeftBottom: function () {
-		return new CPoint(this.left, this.bottom);
-	}
-	, getRightTop: function () {
-		return new CPoint(this.right, this.top);
-	}
-	, getRightBottom: function () {
-		return new CPoint(this.right, this.bottom);
-	}
-	, getCenter: function () {
-		return new CPoint(this.left + this.width / 2, this.top + this.height / 2);
-	}
-	, getDimensions: function () {
-		return new CPoint(this.width, this.height);
-	}
-	, doesContainPoint: function (p, tolerance) {
-		tolerance = tolerance || 0;
-		return (p.x - this.left >= tolerance && this.right - p.x >= tolerance)
-			&& (p.y - this.right >= tolerance && this.bottom - p.y >= tolerance)
-	}
-	, doesContainRect: function (r, tolerance) {
-		return this.doesContainPoint(r.getLeftTop(), tolerance)
-			&& this.doesContainPoint(r.getRightBottom(), tolerance);
-	}
+    , intersectionWithRect: function (r, tolerance) {
+        var tmp = new CRect(r);
+    
+        if (this.doesContainRect(tmp, tolerance)) return tmp;
+        if (tmp.doesContainRect(this, tolerance)) return new CRect(this);
+        
+        var rt = new CRect(
+            Math.max(this.left, tmp.left),
+            Math.max(this.top, tmp.top),
+            Math.min(this.right, tmp.right),
+            Math.min(this.bottom, tmp.bottom)
+        );
+        
+        return (rt.isValid() ? rt : null);
+    }
 
-	, intersectionWithRect: function (r, tolerance) {
-		if (this.doesContainRect(r, tolerance)) return r;
-		if (r.doesContainRect(this, tolerance)) return this;
-		try {
-			return new CRect(
-				Math.max(this.left, r.left),
-				Math.max(this.top, r.top),
-				Math.min(this.right, r.right),
-				Math.min(this.bottom, r.bottom));
-		} catch (e) {
-			return null;
-		}
-	}
+    , moveBy: function (p) {
+        var tmp = new CPoint(p);
+        this.left += tmp.x;
+        this.right += tmp.x;
+        this.top += tmp.y;
+        this.bottom += tmp.y;
 
-	, moveBy: function (p) {
-		this.left += p.x;
-		this.right += p.x;
-		this.top += p.y;
-		this.bottom += p.y;
+        return this;
+    }
+    , moveTo: function (p) {
+        var d = this.getLeftTop().distanceTo(p);
+        return this.moveBy(d);
+    }
+    // move right / bottom by point x / y
+    // resize keeping left top fixed
+    , resizeBy: function (p) {
+        var tmp = new CPoint(p);
+        this.right += tmp.x;
+        this.width += tmp.x;
+        this.bottom += tmp.y;
+        this.height += tmp.y;
 
-		return this;
-	}
-	, moveTo: function (p) {
-		var d = this.getLeftTop().distanceTo(p);
-		return this.moveBy(d);
-	}
-	// move right / bottom by point x / y
-	// resize keeping left top fixed
-	, resizeByPoint: function (p) {
-		this.right += p.x;
-		this.width += p.x;
-		this.bottom += p.y;
-		this.height += p.y;
+        return this;
+    }
+    // move every corner by half point x / y
+    // resize keeping center fixed
+    , symResizeBy: function (p) {
+        var tmp = new CPoint(p);
+        this.left -= Math.floor(tmp.x / 2);
+        this.right += Math.ceil(tmp.x / 2);
+        this.width += tmp.x;
+        this.top -= Math.floor(tmp.y / 2);
+        this.bottom += Math.ceil(tmp.y / 2);
+        this.height += tmp.y;
 
-		return this;
-	}
-	// move every corner by half point x / y
-	// resize keeping center fixed
-	, symResizeByPoint: function (p) {
-		this.left -= Math.floor(p.x / 2);
-		this.right += Math.ceil(p.x / 2);
-		this.width += p.x;
-		this.top -= Math.floor(p.y / 2);
-		this.bottom += Math.ceil(p.y / 2);
-		this.height += p.y;
-
-		return this;
-	}
-	// scale by a point with x = y
-	// resize keeping left top fixed
-	, scaleBy: function (f) {
-		return this.scaleByPoint(new CPoint(f, f));
-	}
-	// sym scale by a point with x = y
-	// resize keeping center fixed
-	, symScaleBy: function (f) {
-		return this.symScaleByPoint(new CPoint(f, f));
-	}
-	// divide size by point x / y then resize
-	// resize keeping left top fixed
-	, scaleByPoint: function (p) {
-		var dx = this.width - (this.width / p.x);
-		var dy = this.height - (this.height / p.y);
-		return this.resizeByPoint(new CPoint(-dx, -dy));
-	}
-	// divide size by point x / y then sym resize
-	// resize keeping center fixed
-	, symScaleByPoint: function (p) {
-		var dx = this.width - (this.width / p.x);
-		var dy = this.height - (this.height / p.y);
-		return this.symResizeByPoint(new CPoint(-dx, -dy));
-	}
-	// multiply sizes by factor
-	, zoomBy: function (f) {
-		return this.zoomByPoint(new CPoint(f, f));
-	}
-	// multiply size by point x / y
-	, zoomByPoint: function (p) {
-		$w('left right width').each(function (w) {
-			this[w] = this[w] * p.x
-		}, this);
-		$w('top bottom height').each(function (w) {
-			this[w] = this[w] * p.y
-		}, this);
-		return this;
-	}
+        return this;
+    }
+    // divide size by point x / y then
+    // resize keeping left top fixed
+    , scaleBy: function (p) {
+        var tmp = new CPoint(p);
+        var dx = this.width - (this.width / tmp.x);
+        var dy = this.height - (this.height / tmp.y);
+        return this.resizeBy([-dx, -dy]);
+    }
+    // divide size by point x / y then
+    // sym resize keeping center fixed
+    , symScaleBy: function (p) {
+        var tmp = new CPoint(p);
+        var dx = this.width - (this.width / tmp.x);
+        var dy = this.height - (this.height / tmp.y);
+        return this.symResizeBy([-dx, -dy]);
+    }
+    // multiply sizes by factor
+    , zoomBy: function (f) {
+        var p = new CPoint(f);
+        $w('left right width').each(function (w) {
+            this[w] = this[w] * p.x
+        }, this);
+        $w('top bottom height').each(function (w) {
+            this[w] = this[w] * p.y
+        }, this);
+        return this;
+    }
 });
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * 
 // rect for viewport
 var CViewportRect = Class.create(CRect, {
-	initialize: function ($super) {
-		var d = document.viewport.getDimensions();
-		var o = document.viewport.getScrollOffsets();
-		$super([0, 0], [d.width + o[0], d.height + o[1]]);
-	}
+    initialize: function ($super) {
+        var d = document.viewport.getDimensions();
+        var o = document.viewport.getScrollOffsets();
+        $super([0, 0], [d.width + o[0], d.height + o[1]]);
+    }
 });
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * 
 // rect with right / bottom margin
 var CMarginRect = Class.create(CRect, {
-	initialize: function ($super) {
-		$super.apply(this, $A(arguments).slice(1));
-		this.refreshMargins = function () {
-			var docDims = document.viewport.getDimensions();
+    initialize: function ($super) {
+        $super.apply(this, $A(arguments).slice(1));
+        this.refreshMargins = function () {
+            var docDims = document.viewport.getDimensions();
 
-			this.marginRight = docDims.width - this.right;
-			this.marginBottom = docDims.height - this.bottom;
+            this.marginRight = docDims.width - this.right;
+            this.marginBottom = docDims.height - this.bottom;
 
-			return this;
-		};
-		this.refreshMargins();
-	}
+            return this;
+        };
+        this.refreshMargins();
+    }
 
-	, moveBy: function ($super, p) {
-		$super(p);
-		return this.refreshMargins();
-	}
-	, resizeByPoint: function ($super, p) {
-		$super(p);
-		return this.refreshMargins();
-	}
-	, symResizeByPoint: function ($super, p) {
-		$super(p);
-		return this.refreshMargins();
-	}
-	, getViewportOverflow: function () {
+    , moveBy: function ($super, p) {
+        $super(p);
+        return this.refreshMargins();
+    }
+    , resizeBy: function ($super, p) {
+        $super(p);
+        return this.refreshMargins();
+    }
+    , symResizeBy: function ($super, p) {
+        $super(p);
+        return this.refreshMargins();
+    }
+    , getViewportOverflow: function () {
 
-		var rt = $H({});
+        var rt = $H({});
 
-		if (this.top < 0) {
-			rt.set("top", new CPoint(0, this.top));
-		}
-		if (this.marginBottom < 0) {
-			rt.set("bottom", new CPoint(0, -this.marginBottom));
-		}
-		if (this.left < 0) {
-			rt.set("left", new CPoint(this.left, 0));
-		}
-		if (this.marginRight < 0) {
-			rt.set("right", new CPoint(-this.marginRight, 0));
-		}
+        if (this.top < 0) {
+            rt.set("top", new CPoint(0, this.top));
+        }
+        if (this.marginBottom < 0) {
+            rt.set("bottom", new CPoint(0, -this.marginBottom));
+        }
+        if (this.left < 0) {
+            rt.set("left", new CPoint(this.left, 0));
+        }
+        if (this.marginRight < 0) {
+            rt.set("right", new CPoint(-this.marginRight, 0));
+        }
 
-		[['top', 'bottom'], ['left', 'right']].each(function (pair) {
-			if (rt.get(pair[0]) != null && rt.get(pair[1]) != null) {
-				console.warn('overflow clash: ' + pair[0] + ' + ' + pair[1]);
-				rt.unset(pair[0]); rt.unset(pair[1]);
-			}
-		});
-		return (rt.size() ? rt : null);
-	}
-	, getStyle: function () {
-		return {
-			left: this.left + "px"
-			, top: this.top + "px"
-			, height: this.height + "px"
-			, width: this.width + "px"
-		};
-	}
+        [['top', 'bottom'], ['left', 'right']].each(function (pair) {
+            if (rt.get(pair[0]) != null && rt.get(pair[1]) != null) {
+                console.warn('overflow clash: ' + pair[0] + ' + ' + pair[1]);
+                rt.unset(pair[0]); rt.unset(pair[1]);
+            }
+        });
+        return (rt.size() ? rt : null);
+    }
+    , getStyle: function () {
+        return {
+            left: this.left + "px"
+            , top: this.top + "px"
+            , height: this.height + "px"
+            , width: this.width + "px"
+        };
+    }
 });
---
+++ geometry/CRect.md
@@ -0,0 +1,203 @@
+# BitOrchestra's prototypejs extensions
+
+## CRect : CClonable
+
+Class representing a geometric rectangle. IS-A [CClonable](../clonable/CClonable.md).
+
+This class is specialized in [CViewportRect](CViewportRect.md), [CMarginRect](CMarginRect.md) and [CHtmlRect](CHtmlRect.md).
+
+Its boundaries are represented by numeric properties: <code>left</code>, <code>top</code>, <code>right</code>, <code>bottom</code>.
+Also its <code>width</code> and <code>height</code> are computed and kept in sync across calls to any mutator method below.
+
+### initialize(element)
+### initialize(CRect)
+### initialize([left,top],[width,height])
+### initialize(width,height)
+### initialize(CPoint,CPoint)
+### initialize(left,top,right,bottom)
+
+**description**
+
+Class constructor: may be called with one, two or four parameters, with following semantics:
+* if one parameters is provided, that must be an element whose layout will be inspected, or a <code>CRect</code> equivalent object with <code>left</code>, <code>top</code>, <code>width</code>, <code>height</code> own properties to copy from
+* if two parameters are provided, they must be 
+ * two arrays containing left-top coordinates and rect size respectively
+ * two numbers describing rect size, with top-left corner assumed to be the origin <code>[0,0]</code>
+ * two [CPoint](CPoint.md)s or equivalent objects with <code>x</code> and <code>y</code> own properties, representing left-top and right-bottom corners respectively
+* if four parameters are provided, they must be numbers describing the two rectangle corners
+
+--
+
+### rect.toString() -> String
+
+**description**
+
+Returns a string representation of current instance formatted as <code>R[[left,top],[right,bottom]]</code>
+
+--
+
+### rect.isValid() -> Boolean
+
+**description**
+
+Tells if current instance is a valid rectangle, i.e. one with non-negative <code>height</code> and <code>width</code>.
+
+--
+
+### rect.getLeftTop() -> CPoint
+### rect.getLeftBottom() -> CPoint
+### rect.getRightTop() -> CPoint
+### rect.getRightBottom() -> CPoint
+
+**description**
+
+Returns vertices as [CPoint](CPoint.md) instances.
+
+--
+
+### rect.getCenter() -> CPoint
+
+**description**
+
+Returns the center of this rectangle as a [CPoint](CPoint.md) instance.
+
+--
+
+### rect.getDimensions() -> CPoint
+
+**description**
+
+Returns the size of this rectangle as a [CPoint](CPoint.md) instance.
+
+--
+
+### rect.doesContainPoint(point[, tolerance = 0]) -> Boolean
+
+**description**
+
+Tells if the <code>point</code> argument (any value suitable to construct a [CPoint](CPoint.md)s) is contained in this rectangle instance,
+respecting the given <code>tolerance</code>.
+
+--
+
+### rect.doesContainRect(rect[, tolerance = 0]) -> Boolean
+
+**description**
+
+Tells if the <code>rect</code> argument (any value suitable to construct a <code>CRect</code>) is contained in this rectangle instance,
+respecting the given <code>tolerance</code>.
+
+--
+
+### rect.intersectionWithRect(rect[, tolerance = 0]) -> CRect
+
+**description**
+
+Returns the rectangle representing the intersection between current instance and the <code>rect</code> argument. <code>tolerance</code> is used
+to perform inclusion check, i.e. to discover if one rectangle is inside the other.
+
+Returned value is always a new rectangle, even if the intersection corresponds to current instance.
+
+--
+
+### rect.moveBy(point) -> CRect
+
+**description**
+
+Moves current rectangle around by the amount specified by the <code>point</code> argument (any value suitable to construct a [CPoint](CPoint.md)s),
+changing values of current instance properties. 
+
+<code>this</code> is returned to provide method chaining.
+
+--
+
+### rect.moveTo(point) -> CRect
+
+**description**
+
+Moves current rectangle so that its top-left corner will be at coordinates specified by the <code>point</code> argument (any value suitable to construct a [CPoint](CPoint.md)s),
+changing values of current instance properties. 
+
+<code>this</code> is returned to provide method chaining.
+
+--
+
+### rect.resizeBy(point) -> CRect
+
+**description**
+
+Changes the size of current rectangle by the amount(s) specified by the <code>point</code> argument (any value suitable to construct a [CPoint](CPoint.md)s),
+changing values of current instance properties; the top-left corner remains in its position, while the bottom-right will be moved.
+
+<code>this</code> is returned to provide method chaining.
+
+--
+
+### rect.symResizeBy(point) -> CRect
+
+**description**
+
+Changes the size of current rectangle by the amount(s) specified by the <code>point</code> argument (any value suitable to construct a [CPoint](CPoint.md)s),
+changing values of current instance properties; resizing is performed keeping the *center* of the rectangle fixed, so every coordinate will be moved: <code>top</code>
+and <code>bottom</code> by half of the specified <code>y</code> amount, <code>left</code> and <code>right</code> by half of the specified <code>x</code> amount.
+
+<code>this</code> is returned to provide method chaining.
+
+--
+
+### rect.scaleBy(point) -> CRect
+
+**description**
+
+Scales the size of current rectangle by the factor(s) specified by the <code>point</code> argument (any value suitable to construct a [CPoint](CPoint.md)s),
+changing values of current instance properties; the top-left corner remains in its position, while the bottom-right will be moved.
+
+<code>this</code> is returned to provide method chaining.
+
+**example**
+
+```
+var y = new CRect(100,100).moveBy(20);  // R[[20,20],[120,120]]
+y.scaleBy(4);       // R[[20,20],[45,45]]
+console.log(y.getDimensions().toString());  // P[25,25]
+```
+
+--
+
+### rect.symScaleBy(point) -> CRect
+
+**description**
+
+Scales the size of current rectangle by the factor(s) specified by the <code>point</code> argument (any value suitable to construct a [CPoint](CPoint.md)s),
+changing values of current instance properties; scaling is performed keeping the *center* of the rectangle fixed, so every coordinate will be moved.
+
+<code>this</code> is returned to provide method chaining.
+
+**example**
+
+```
+var y = new CRect(100,100).moveBy(20);  // R[[20,20],[120,120]]
+y.symScaleBy(4);       // R[[58,58],[83,83]]
+console.log(y.getDimensions().toString());  // P[25,25]
+```
+
+--
+
+### rect.zoomBy(point) -> CRect
+
+**description**
+
+Multiplies current rectangle properties by the factor(s) specified by the <code>point</code> argument (any value suitable to construct a [CPoint](CPoint.md)s),
+changing values of current instance properties; no geometric property is kept fixed.
+
+<code>this</code> is returned to provide method chaining.
+
+**example**
+
+```
+var y = new CRect(100,100).moveBy(20);  // R[[20,20],[120,120]]
+y.zoomBy(2);       // R[[58,58],[83,83]]
+console.log(y.toString());  // R[[40,40],[240,240]]
+console.log(y.getDimensions().toString());  // P[200,200]
+```
+
